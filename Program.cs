408e8caec4ff8ad93a0dd7f630edf0ec4cc55ef6
Rodrigo <rodrigofnobrega1@gmail.com>
2026-02-27 20:22:54 -0300

    feat: Criar endpoint para retornar versão

--- Program.cs
+++ Program.cs
@@ -5,4 +5,16 @@ app.MapGet("/", () => "Hello World!");
 
 app.MapGet("/soma", () => 2 + 1);
 
+app.MapGet("/version", () => 
+{
+    var version = Assembly.GetEntryAssembly()
+                          ?.GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+                          ?.InformationalVersion;
+                          
+    return Results.Ok(new { 
+        Version = version, 
+        BuildDate = DateTime.UtcNow 
+    });
+});
+
 app.Run();
